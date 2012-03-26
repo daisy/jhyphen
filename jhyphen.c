1612a2d7d562faed4310da93b85d4ec776775350
Bert Frees <bert.frees@sbs.ch>
2012-03-26 15:31:39 +0200

    changed doSomethin to output something useful

--- jhyphen.c
+++ jhyphen.c
@@ -1,20 +1,44 @@
-#include <string.h>
 #include <hyphen.h>
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX_WORD 256
 
 int doSomething(const char* fn, const char* word) {
 
   HyphenDict* dict;
-  char* lcword;
-  char* hyphword;
+  char lcword[MAX_WORD];
+  char hyphword[MAX_WORD];
+  char hyphens[MAX_WORD];
   int word_size;
+  int i;
+  int j;
 
   dict = hnj_hyphen_load(fn);
 
-  lcword = tolower(word);
-  word_size = strlen(lcword);
+  word_size = strlen(word);
 
-  //hnj_hyphen_hyphenate(dict, lcword, word_size, hyphens);
+  for (i = 0; i < word_size; ++i) {
+    lcword[i] = tolower(word[i]);
+  }
+  lcword[i] = '\0';
+  
+  hnj_hyphen_hyphenate(dict, lcword, word_size, hyphens);
 
+  j = 0;
+  for (i = 0; i < word_size; i++) {
+    hyphword[j++] = word[i];
+    if (hyphens[i]&1) {
+      hyphword[j++] = '-';
+    }
+  }
+  hyphword[j] = '\0';
+
+  fprintf(stdout,"%s\n", hyphword);
+  fflush(stdout);
 
   return 1;
 }
