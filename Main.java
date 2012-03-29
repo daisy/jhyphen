5c389f05d6801daf436ea937f617f27d9ef845d6
Christian Egli <christian.egli@alumni.ethz.ch>
2012-03-29 16:57:25 +0200

    Main.java has been moved to the test dir

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-   public static void main(String argv[]) {
-     System.loadLibrary("jhyphen");
-     System.out.println("steamboat");
-     jhyphen.doSomething("/home/frees/dev/hyphen/hyphen.tex", "steamboat");
-   }
- }
