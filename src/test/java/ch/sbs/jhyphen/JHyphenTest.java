a8be8b1873d71440f0de88556a6e7013d9fc05a3
Bert Frees <bertfrees@gmail.com>
2013-09-17 11:39:56 +0200

    Move whitelist test from jhyphen to sbs-hyphenation-tables

--- src/test/java/ch/sbs/jhyphen/JHyphenTest.java
+++ src/test/java/ch/sbs/jhyphen/JHyphenTest.java
@@ -7,15 +7,11 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
-import java.io.IOException;
 import java.nio.charset.UnsupportedCharsetException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Scanner;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
@@ -87,44 +83,6 @@ public class JHyphenTest {
 		for (Entry<String,String> entry : words.entrySet()) {
 			assertEquals(entry.getValue(), hyphenator.hyphenate(entry.getKey(), '=', '='));
 		}
-		
-		hyphenator.close();
-	}
-	
-	@Test
-	public void testWhitelist() throws IOException {
-		
-		File projectHome = new File("/home/frees/dev/sbs-hyphenation-tables/sbs-hyphenation-tables");
-		
-		File whitelist = new File(projectHome, "whitelist_de_SBS.txt");
-		File dictionary = new File(projectHome, "hyph_de_DE.dic");
-		File origDictionary = new File(projectHome, "hyph_de_DE.orig.dic");
-		
-		Hyphenator hyphenator = new Hyphenator(dictionary);
-		Hyphenator origHyphenator = new Hyphenator(origDictionary);
-		
-		List<String> redundantWords = new ArrayList<String>();
-		
-		Scanner scanner = new Scanner(whitelist, "ISO8859-1");
-		long start = System.currentTimeMillis();
-		int i = 0;
-		String unhyphenated = null;
-		String word = null;
-		while (scanner.hasNext()) {
-			word = scanner.nextLine();
-			unhyphenated = word.replaceAll("\\-", "");
-			assertEquals(word, hyphenator.hyphenate(unhyphenated, '-', null));
-			if(word.equals(origHyphenator.hyphenate(unhyphenated, '-', null)))
-				redundantWords.add(unhyphenated);
-			i++;
-			if (i % 100 == 0)
-				System.out.println(String.format("... tested %d words in %d milliseconds",
-						i, (int)(System.currentTimeMillis()-start))); }
-		
-		System.out.print("The following words are correctly hyphenated without whitelist:\n=> ");
-		for (String w : redundantWords)
-			System.out.print(w + " ");
-		System.out.println();
 		
 		hyphenator.close();
 	}
